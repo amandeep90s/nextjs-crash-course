a914eb8716e3c2834b4ecae6fdb1c69f8da22557
Amandeep Singh <amandeep90s@users.noreply.github.com>
2025-10-26 14:41:04 +0530

    feat: Implement event API with GET and POST methods, including error handling and image upload

--- app/events/[slug]/page.tsx
+++ app/events/[slug]/page.tsx
@@ -1,3 +1,14 @@
-export default function EventDetailsPage() {
-  return <div>EventDetailsPage</div>;
+import { EventDetails } from '@/components/EventDetails';
+import { Suspense } from 'react';
+
+export default async function EventDetailsPage({ params }: { params: Promise<{ slug: string }> }) {
+  const slug = params.then(p => p.slug);
+
+  return (
+    <main>
+      <Suspense fallback={<div>Loading...</div>}>
+        <EventDetails params={slug} />
+      </Suspense>
+    </main>
+  );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,45 @@
-export default function Home() {
+import { EventCard } from '@/components/EventCard';
+import { ExploreBtn } from '@/components/ExploreBtn';
+import { IEvent } from '@/database';
+import { cacheLife } from 'next/cache';
+
+const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
+
+export default async function Home() {
+  'use cache';
+  cacheLife('hours');
+  const response = await fetch(`${BASE_URL}/api/events`);
+  const { events } = await response.json();
+
   return (
     <section>
       <h1 className='text-center'>
         The Hub for Every Dev <br /> Event You Can&apos;t Miss
       </h1>
       <p className='mt-5 text-center'>Hackathons, Meetups, and Conferences, All in One Place</p>
+
+      <ExploreBtn />
+
+      <div className='mt-20 space-y-7'>
+        <h3>Featured Events</h3>
+
+        <ul className='events'>
+          {events &&
+            events.length > 0 &&
+            events.map((event: IEvent) => (
+              <li key={event.title} className='list-none'>
+                <EventCard
+                  title={event.title}
+                  image={event.image}
+                  slug={event.slug}
+                  location={event.location}
+                  date={event.date}
+                  time={event.time}
+                />
+              </li>
+            ))}
+        </ul>
+      </div>
     </section>
   );
 }
